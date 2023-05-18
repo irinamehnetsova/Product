51637d7f145c3d3a036ad6f6bcfcbf8f8358974c
IrinaMehnetsova <i7417m@gmail.com>
2023-05-18 16:23:48 +0300

    Inihial commit

---
+++ src/test/java/org/example/ProductManagerTest.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+    @Test
+
+    public void shouldSave() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Book book1 = new Book(222, "Тихий Дон", 1500, "М.Шолохов");
+        Book book2 = new Book(333, "Собачье сердце", 2300, "М.Булгаков");
+        Book book3 = new Book(444, "В окопах Сталинграда", 900, "В.Некрасов");
+
+        manager.save(book1);
+        manager.save(book2);
+        manager.save(book3);
+        Product[] expected = {book1, book2, book3};
+        Product[] actual = repository.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldFindByText() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Book book1 = new Book(222, "Война и Мир", 1500, "Л.Толстой");
+        Book book2 = new Book(333, "Война и Мир", 2300, "Л.Толстой");
+        Book book3 = new Book(444, "В окопах Сталинграда", 900, "В.Некрасов");
+
+        manager.save(book1);
+        manager.save(book2);
+        manager.save(book3);
+        Product[] expected = {book1, book2};
+        Product[] actual = manager.searchBy("Война и Мир");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindByName() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Book book1 = new Book(222, "Тихий Дон", 1500, "М.Шолохов");
+        Book book2 = new Book(333, "Собачье сердце", 2300, "М.Булгаков");
+        Book book3 = new Book(444, "В окопах Сталинграда", 900, "В.Некрасов");
+
+        manager.save(book1);
+        manager.save(book2);
+        manager.save(book3);
+        Product[] expected = {book2};
+        Product[] actual = manager.searchBy( "Собачье сердце");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void nothingFind() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Book book1 = new Book(222, "Тихий Дон", 1500, "М.Шолохов");
+        Book book2 = new Book(333, "Собачье сердце", 2300, "М.Булгаков");
+        Book book3 = new Book(444, "В окопах Сталинграда", 900, "В.Некрасов");
+
+        manager.save(book1);
+        manager.save(book2);
+        manager.save(book3);
+        Product[] expected = {};
+        Product[] actual = manager.searchBy( "Собачье Cердце");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+}
